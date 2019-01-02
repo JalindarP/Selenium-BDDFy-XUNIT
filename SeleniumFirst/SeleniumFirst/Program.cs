08cc52c7a2681036775f7ef41afc66ff7e0a29aa
Jalindar Pandharkar (OWQ8) <jalindarpandharkar@gmail.com>
2019-01-02 17:13:45 +0530

    no message

---
+++ SeleniumFirst/SeleniumFirst/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver(@"C:\Users\owq8\source\repos\SeleniumFirst\SeleniumFirst\bin\Debug\netcoreapp2.2");
+            driver.Navigate().GoToUrl("https://www.linkedin.com/");
+            IWebElement element = driver.FindElement(By.Id("login-email"));
+            element.SendKeys("mymail@gmail.com");
+
+            element = driver.FindElement(By.Id("login-password"));
+            element.SendKeys("password");
+
+            element = driver.FindElement(By.Id("login-submit"));
+            element.Submit();
+
+
+
+        }
+    }
+}
